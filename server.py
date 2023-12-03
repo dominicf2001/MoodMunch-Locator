4a39b4887a0bbeedfec0f77a7480b35864319275
Dominic <dominicf2001@gmail.com>
2023-12-03 13:07:30 -0500

    html and css work

--- server.py
+++
@@ -1,9 +0,0 @@
-from location_mood_recommendation import food_result
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    print("Hello world")
-    return ""
