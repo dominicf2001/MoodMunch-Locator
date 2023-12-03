a3d3a620f06ba3ee44a36e954aa96bb8b620a504
Dominic <dominicf2001@gmail.com>
2023-12-03 13:26:21 -0500

    implement basic connection to recomendation script

---
+++ app.py
@@ -0,0 +1,20 @@
+from location_mood_recommendation import food_result
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+emotions = ["stressed", "sad", "happy", "bored"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", emotions=emotions);
+
+@app.route("/emotions/<emotion>")
+def handle_emotion(emotion):
+    top_foods = food_result(emotion)
+
+    food_list = ""
+    for food in top_foods:
+        food_list += f"<li>{food}</li>\n"
+    
+    return food_list;
